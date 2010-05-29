b1642c3c0e5c4b8796f4c8109fb89f80dd585b73
Matti Linjama <RYWeb@.(none)>
2010-05-29 22:06:22 +0300

    etusivun_tekstiversio

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,9 @@
 class MainController < ApplicationController
+  
+  # Controllers for individual pages
+
   def index
+    @title = "Kaunis Veranta - Puutarhasuunnitelmat ja sisustustekstiilit (Pirkanmaa)"
   end
 
 end
