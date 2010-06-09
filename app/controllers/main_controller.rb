24cc164fe738fc52c35220517a38ef39cf936d87
Matti Linjama <RYWeb@.(none)>
2010-06-09 22:02:17 +0300

    CSS taitto, sisustussivu, sisatoa

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,10 +4,20 @@ class MainController < ApplicationController
 
   def index
     @page_title = "Kaunis Veranta - Puutarhasuunnitelmat ja sisustustekstiilit (Pirkanmaa)"
+    @top_corner_image = "puutarha_small.jpg"
+    @top_corner_image_options = {:size =>"120x186", :alt => "Istutusryhmä", :class => "top_corner_image"}
   end
   
   def puutarha
-    @page_title = "Puutarhasuunnitelmat: Helppohoitoinen ja kaunis piha"
+    @page_title = "Puutarhasuunnitelmat - Helppohoitoinen ja kaunis piha"
+    @top_corner_image = "puutarha_small.jpg"
+    @top_corner_image_options = {:size =>"120x186", :alt => "Istutusryhmä", :class => "top_corner_image"}
+  end
+  
+  def sisustus
+    @page_title = "Sisustustekstiilit - Kaunis koti"
+    @top_corner_image = "puutarha_small.jpg"
+    @top_corner_image_options = {:size =>"120x186", :alt => "Istutusryhmä", :class => "top_corner_image"}
   end
 
 end
